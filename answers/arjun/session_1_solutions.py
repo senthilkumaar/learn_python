75d32825b5a8e2ce915d0f80306b1eacf5f9a44e
arjung123 <52073667+arjung123@users.noreply.github.com>
2019-06-21 18:20:41 +0530

    Update session_1_solutions.py

--- answers/arjun/session_1_solutions.py
+++ answers/arjun/session_1_solutions.py
@@ -1,3 +1,30 @@
 1. Python is dynamically typed
 2.Data types are Dynamically typed which means data type is automatically detected 
-and interpreted
+and interpreted 
+As it is dynamically typed, will consume some nano seconds to interpret. but it is negligible
+
+ 3. An integer is a number without a decimal point. A float is a floating-point number, which means it is a number that has a decimal place
+
+4. List, dictionary, tuple
+
+5. List
+
+6. type(variable)
+
+7. text='Hello World'
+	print(text)
+
+8. special_string=' Python is an "object-oriented" Programming language'
+	print(special_string)
+
+9.multiline_string="""Unlike other high level languages
+
+		Python is interpreted"""
+	print(multiline_string)
+
+10. news='Air travel to become bit costlier as govt announces hike in aviation security fee from July 1'
+	print(news[0:10])
+	print(news[-1:-7])
+
+ 
+
